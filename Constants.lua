ad1671be818b1d69a5825f3a30fa749f435545ee
Kruithne <kruithne@gmail.com>
2017-07-20 02:52:36 +0100

    Remove unused slug constant

--- Constants.lua
+++ Constants.lua
@@ -2,7 +2,6 @@ do
 	-- This initialized the string table with constant values that don't need localizing.
 	Shame.strings = {
 		ADDON_NAME = "Shame",
-		ADDON_SLUG = "shame",
 
 		CHAT_PREFIX = "|cffff996f[Shame]|r |cffaeebff%s|r",
 		FORMAT_COMMAND = "/shame %s|cfff58cba%s|r",
